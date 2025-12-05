41e9bf5b7cd29a6f86841cc9619ae9a56dc41e21
Branford Coleman <branford2001@gmail.com>
2025-12-04 20:04:19 -0800

    Part 2 done

--- day1/main.cpp
+++ day1/main.cpp
@@ -18,7 +18,7 @@ int solutionPart1(char *inputPath) {
         while (std::getline(inputFile, line)) {
             direction = (line[0] == 'L' ? -1 : 1);
             rotation = atoi(line.substr(1).c_str()) * direction;
-            position = (position + rotation + 100) % 100;
+            position = (position + (rotation % 100) + 100) % 100;
             std::cout << position << "\n";
             result += (position == 0 ? 1 : 0);
         }
@@ -31,9 +31,11 @@ int solutionPart2(char *inputPath) {
     int result = 0;
     std::string line;
     auto inputFile = std::ifstream(inputPath);
+    int clicksDuringRotation = 0;
+    int clicksAtEnd = 0;
 
     // Initial conditions of the lock
-    int position = 50, prevPosition = -0, rotation = 0, direction = 0;
+    int position = 50, prevPosition = 50, rotation = 0, direction = 0;
 
     if (!inputFile.is_open()) {
         std::cout << "Error: File does not exist\n";
@@ -42,22 +44,43 @@ int solutionPart2(char *inputPath) {
         while (std::getline(inputFile, line)) {
             direction = (line[0] == 'L' ? -1 : 1);
             rotation = atoi(line.substr(1).c_str()) * direction;
-            position = (position + rotation + 100) % 100;
-            std::cout << position << "\n";
+            position = (position + (rotation % 100) + 100) % 100;
 
-            if (direction == -1) {
-                if (prevPosition > position) {
-                    result += 1;
+            // Count number of clicks from very large rotations
+            while (abs(rotation) > 100) {
+                if (rotation > 0) {
+                    rotation -= 100;
+                } else {
+                    rotation += 100;
                 }
-            } else if (prevPosition < position) {
-                result += 1;
+                clicksDuringRotation++;
             }
 
-            // Figure out how many times 0 was passed due to excess rotation
-            result += (rotation) / 100;
+            // Only consider these cases if previous position was not 0. This
+            // prevents duplicate clicks.
+            if (prevPosition != 0) {
+                if (position == 0) {
+                    std::cout << line << " click... (reached 0)\n";
+                    clicksAtEnd++;
+                } else if (direction == -1 && (position > prevPosition)) {
+                    std::cout << line << " click...\n";
+                    clicksDuringRotation++;
+                } else if (direction == 1 && (position < prevPosition)) {
+                    std::cout << line << " click...\n";
+                    clicksDuringRotation++;
+                }
+            }
+
+            std::cout << "previous position " << prevPosition << "\n";
+            std::cout << "position " << position << "\n";
+            std::cout << "..........................\n";
+            prevPosition = position;
         }
     }
     inputFile.close();
+    result += clicksAtEnd + clicksDuringRotation;
+    std::cout << "CLICKS DURING ROTATION : " << clicksDuringRotation
+              << "\tCLICKS AT END: " << clicksAtEnd << "\n";
     return result;
 }
 int main(int argc, char **argv) {
